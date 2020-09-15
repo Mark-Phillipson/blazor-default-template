a98ce1c195e4a31101d7d079bd997f8c4f6bcee4
Mark Phillipson <MPhillipson0@Gmail.com>
2020-09-15 11:58:45 +0100

    Reformatted dictation box for mobile view

---
+++ MSPToDoList/Pages/Dictationvalue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSPToDoList.Pages
+{
+    public class Dictationvalue
+    {
+        public string DictationBoxValue { get; set; }
+
+    }
+}
---
+++ MSPToDoList/Shared/CopyToClipboard.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSPToDoList.Shared
+{
+    public partial class CopyToClipboard
+    {
+        [Parameter] public int Rows { get; set; }
+
+        public string Text { get; set; }
+        public string Result { get; set; }
+        private async Task CopyTextToClipboard()
+        {
+            await JavascriptRuntime.InvokeVoidAsync(
+                "clipboardCopy.copyText", Text);
+            Result = $"Copied Successfully at {DateTime.Now.ToString("hh:mm")}";
+        }
+        private async Task ClearDictationAsync()
+        {
+            Text = null;
+            Result = null;
+            await JavascriptRuntime.InvokeVoidAsync("setFocus","DictationBox");
+        }
+    }
+}
