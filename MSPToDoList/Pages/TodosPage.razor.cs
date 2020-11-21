f4fe97f1c9db8cff4ece85461065d1faf6591565
Mark Phillipson <MPhillipson0@Gmail.com>
2020-11-21 16:07:53 +0000

    code formatting changes only

--- MSPToDoList/Pages/TodosPage.razor.cs
+++ MSPToDoList/Pages/TodosPage.razor.cs
@@ -15,102 +15,102 @@ using System.Text.Json;
 
 namespace MSPToDoList.Pages
 {
-    public partial class TodosPage
-    {
-        private List<ToDoList> todos;
-        [Inject]
-        public ILocalStorageService LocalStorage { get; set; }
-        private IFileListEntry file;
-#pragma warning disable 414, 649,169
-        private string message = "";
-        private bool _loadFailed = false;
-        ElementReference SearchInput;
-#pragma warning restore 414, 649,169
-        private string SearchTerm { get; set; }
-        public bool ShowCompleted { get; set; } = true;
-        protected override async Task OnInitializedAsync()
-        {
-            await LoadData();
-        }
+	public partial class TodosPage
+	{
+		private List<ToDoList> todos;
+		[Inject]
+		public ILocalStorageService LocalStorage { get; set; }
+		private IFileListEntry file;
+#pragma warning disable 414, 649, 169
+		private string message = "";
+		private bool _loadFailed = false;
+		ElementReference SearchInput;
+#pragma warning restore 414, 649, 169
+		private string SearchTerm { get; set; }
+		public bool ShowCompleted { get; set; } = true;
+		protected override async Task OnInitializedAsync()
+		{
+			await LoadData();
+		}
 
-        private async Task LoadData()
-        {
-            todos = await LocalStorage.GetItemAsync<List<ToDoList>>("todo");
-            if (todos == null || todos.Count == 0)
-            {
-                todos = await Http.GetFromJsonAsync<List<ToDoList>>("sample-data/todo.json");
-            }
-        }
+		private async Task LoadData()
+		{
+			todos = await LocalStorage.GetItemAsync<List<ToDoList>>("todo");
+			if (todos == null || todos.Count == 0)
+			{
+				todos = await Http.GetFromJsonAsync<List<ToDoList>>("sample-data/todo.json");
+			}
+		}
 
-        protected async Task SaveToDoAsync()
-        {
-            await LocalStorage.SetItemAsync<List<ToDoList>>("todo", todos);
-            //message = $"Saved! {DateTime.Now.TimeOfDay.ToString("hh:nn")}";
-            toastService.ShowSuccess("All to dos have been saved successfully!");
-        }
+		protected async Task SaveToDoAsync()
+		{
+			await LocalStorage.SetItemAsync<List<ToDoList>>("todo", todos);
+			//message = $"Saved! {DateTime.Now.TimeOfDay.ToString("hh:nn")}";
+			toastService.ShowSuccess("All to dos have been saved successfully!");
+		}
 
-        private string title { get; set; } = "Todo List";
-        //usersJson = LoadJson(@"services\users.json");
-        //users = JsonConvert.DeserializeObject<List<AspNetUser>>(usersJson);
+		private string title { get; set; } = "Todo List";
+		//usersJson = LoadJson(@"services\users.json");
+		//users = JsonConvert.DeserializeObject<List<AspNetUser>>(usersJson);
 
-        private string LoadJson(string fileName)
-        {
-            using (StreamReader reader = new StreamReader(fileName))
-            {
-                string json = reader.ReadToEnd();
-                return json;
-            }
-        }
-        private async Task AddToDoAsync()
-        {
-            ToDoList toDoList = new ToDoList { DateCreated = DateTime.Now.Date };
-            todos.Add(toDoList);
-            await JSRuntime.InvokeVoidAsync("setFocus", $"{toDoList.Id}TitleInputBig");
-        }
-        private void DeleteToDo(ToDoList toDoList)
-        {
-            todos.Remove(toDoList);
-        }
+		private string LoadJson(string fileName)
+		{
+			using (StreamReader reader = new StreamReader(fileName))
+			{
+				string json = reader.ReadToEnd();
+				return json;
+			}
+		}
+		private async Task AddToDoAsync()
+		{
+			ToDoList toDoList = new ToDoList { DateCreated = DateTime.Now.Date };
+			todos.Add(toDoList);
+			await JSRuntime.InvokeVoidAsync("setFocus", $"{toDoList.Id}TitleInputBig");
+		}
+		private void DeleteToDo(ToDoList toDoList)
+		{
+			todos.Remove(toDoList);
+		}
 
-        private async Task CallChangeAsync(string elementId)
-        {
-            await JSRuntime.InvokeVoidAsync("CallChange", elementId);
-        }
-        private async Task ReloadTodosAsync()
-        {
-            await LoadData();
-        }
-        async Task DownloadFileAsync()
-        {
-            //var text = todos.ToList().ToString();
-            var text = JsonConvert.SerializeObject(todos.ToList());
-            var bytes = System.Text.Encoding.UTF8.GetBytes(text);
-            await FileUtility.SaveAs(JSRuntime, "todo.json", bytes);
+		private async Task CallChangeAsync(string elementId)
+		{
+			await JSRuntime.InvokeVoidAsync("CallChange", elementId);
+		}
+		private async Task ReloadTodosAsync()
+		{
+			await LoadData();
+		}
+		async Task DownloadFileAsync()
+		{
+			//var text = todos.ToList().ToString();
+			var text = JsonConvert.SerializeObject(todos.ToList());
+			var bytes = System.Text.Encoding.UTF8.GetBytes(text);
+			await FileUtility.SaveAs(JSRuntime, "todo.json", bytes);
 
-        }
-        // https://blog.stevensanderson.com/2019/09/13/blazor-inputfile/
-        async Task HandleFileSelectedAsync(IFileListEntry[] files)
-        {
-            file = files.FirstOrDefault();
-            List<ToDoList> todosImported;
-            using (var reader = new StreamReader(file.Data))
-            {
-                try
-                {
-                    todosImported = JsonConvert.DeserializeObject<List<ToDoList>>( await reader.ReadToEndAsync());
-                    if (todosImported.Count > 0)
-                    {
-                        foreach (var todo in todosImported)
-                        {
-                            todos.Add(todo);
-                        }
-                    }
-                }
-                catch (Exception exception)
-                {
-                    message = exception.Message;
-                }
-            }
-        }
-    }
+		}
+		// https://blog.stevensanderson.com/2019/09/13/blazor-inputfile/
+		async Task HandleFileSelectedAsync(IFileListEntry[] files)
+		{
+			file = files.FirstOrDefault();
+			List<ToDoList> todosImported;
+			using (var reader = new StreamReader(file.Data))
+			{
+				try
+				{
+					todosImported = JsonConvert.DeserializeObject<List<ToDoList>>(await reader.ReadToEndAsync());
+					if (todosImported.Count > 0)
+					{
+						foreach (var todo in todosImported)
+						{
+							todos.Add(todo);
+						}
+					}
+				}
+				catch (Exception exception)
+				{
+					message = exception.Message;
+				}
+			}
+		}
+	}
 }
